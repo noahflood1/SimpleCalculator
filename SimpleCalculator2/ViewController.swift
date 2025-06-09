397000181213ec54c77e85bd251e7fb06c602938
Noah Flood <noahflood12@gmail.com>
2025-06-08 23:22:43 -0500

    Initial Commit

---
+++ SimpleCalculator2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator2
+//
+//  Created by Noah Flood on 6/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleCalculator2Tests/SimpleCalculator2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCalculator2Tests.swift
+//  SimpleCalculator2Tests
+//
+//  Created by Noah Flood on 6/8/25.
+//
+
+import Testing
+@testable import SimpleCalculator2
+
+struct SimpleCalculator2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
